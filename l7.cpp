b143112f201d5ba601d5e1c41d324de51c6685fe
jakebracey <jakebracey62@gmail.com>
2016-10-25 13:21:46 -0500

    everything is working, need to add comments

--- l7.cpp
+++ l7.cpp
@@ -16,21 +16,7 @@
 #include <cstring>
 
 using namespace std;
-
-int isNumeric (const char * s);
-//int *load_array(char* file, int* length, int* max_val);
-
-/*
-These are the functions that were used before converting this code to C++
-
-void do_offset(int* array, int* length, char*file, double val);
-void do_scale(int* array, int* length, char*file,double val);
-double getmean(int[], int*);
-int getmax(int[], int*);
-void write_stats(char[], int[], int*);
-void do_center(int[], int*, char*);
-void do_normal(int[], int*, char*);
-*/
+int result=0;
 
 class Signal{
 	private:
@@ -44,9 +30,9 @@ class Signal{
 			Signal();
 			Signal(int);
 			Signal(const char*);
-			Signal builder();
-			Signal builder(int file_sel);
-			Signal builder(const char* file_name);
+			Signal& builder();
+			Signal& builder(int file_sel);
+			Signal& builder(const char* file_name);
 			~Signal();
 			void offset(double num);
 			void scale(double num);
@@ -58,17 +44,18 @@ class Signal{
 			void Save_file(const char*);
 };
 
-Signal Signal::builder(){
+Signal& Signal::builder(){
 	FILE* fp = fopen("Raw_data_01.txt", "r");
 	//opens a default input file for reading
-
+	int dum;
 	if (fp == NULL) { //making sure the default input file opens correctly
-		printf("Error opening the default input file Raw_data_01.txt");
-		return 0;
-		//terminates program
+		cout<<"Error opening the default input file Raw_data_01.txt"<<endl;
+		result=0;
 	}
-
+	
+	else{
 	fscanf(fp, "%d %d", &length, &max);
+	result=1;
 
 	data = new double[length];
 	//mallocs space for the array
@@ -76,15 +63,17 @@ Signal Signal::builder(){
 	int i = 0;
 	for (i = 0; i < length; i++) {
 		//for loop to put the values into the array
-		fscanf(fp, "%d", (data + i));
+		fscanf(fp, "%d", (&dum));
+		data[i]=(double)dum;
 	}
 	fclose(fp);
-	printf("before reutnr\n");
+	}
 	return *this;
 }
 
-Signal Signal::builder(int file_sel){
+Signal& Signal::builder(int file_sel){
 	char file_name[66];
+	int dum;
 	if ( file_sel< 10)
 		sprintf(file_name, "Raw_data_0%d.txt", file_sel);
 		else
@@ -94,11 +83,12 @@ Signal Signal::builder(int file_sel){
 	//opens the given input file for reading
 
 	if (fp == NULL) { //making sure the default input file opens correctly
-		printf("Error opening the default input file Raw_data_01.txt");
-		return 0;
-		//terminates program
+		cout<<"Error opening the default input file "<<file_name<<endl;
+		result=0;
 	}
 
+	else{
+	result=1;
 	fscanf(fp, "%d %d", &length, &max);
 
 	data = new double[length];
@@ -107,22 +97,25 @@ Signal Signal::builder(int file_sel){
 	int i = 0;
 	for (i = 0; i < length; i++) {
 		//for loop to put the values into the array
-		fscanf(fp, "%d", (data + i));
+		fscanf(fp, "%d", (&dum));
+		data[i]=(double)dum;
 	}
 	fclose(fp);
+	}
 	return *this;
 }
 
-Signal Signal::builder(const char* file_name){
+Signal& Signal::builder(const char* file_name){
 	FILE* fp = fopen(file_name, "r");
+	int dum;
 	//opens the given input file for reading
 
 	if (fp == NULL) { //making sure the default input file opens correctly
-		printf("Error opening the default input file Raw_data_01.txt");
-		return 0;
-		//terminates program
+		cout<<"Error opening the default input file "<<file_name<<endl;
+		result=0;
 	}
-
+	else{
+	result=1;
 	fscanf(fp, "%d %d", &length, &max);
 
 	data = new double[length];
@@ -131,9 +124,11 @@ Signal Signal::builder(const char* file_name){
 	int i = 0;
 	for (i = 0; i < length; i++) {
 		//for loop to put the values into the array
-		fscanf(fp, "%d", (data + i));
+		fscanf(fp, "%d", (&dum));
+		data[i]=(double)dum;
 	}
 	fclose(fp);
+	}
 	return *this;
 
 }
@@ -151,32 +146,7 @@ Signal::Signal(){
 }
 
 Signal::Signal(int file_sel){
-	char file_name[66];
-	if ( file_sel< 10)
-		sprintf(file_name, "Raw_data_0%d.txt", file_sel);
-		else
-		sprintf(file_name, "Raw_data_%d.txt", file_sel);
-	
-	FILE* fp = fopen(file_name, "r");
-	//opens the given input file for reading
-
-	if (fp == NULL) { //making sure the default input file opens correctly
-		printf("Error opening the default input file Raw_data_01.txt");
-		//return 0;
-		//terminates program
-	}
-
-	fscanf(fp, "%d %d", &length, &max);
-
-	data = new double[length];
-	//mallocs space for the array
-
-	int i = 0;
-	for (i = 0; i < length; i++) {
-		//for loop to put the values into the array
-		fscanf(fp, "%d", (data + i));
-	}
-	fclose(fp);
+	builder(file_sel);
 }
 
 Signal::Signal(const char* file_name){
@@ -184,9 +154,8 @@ Signal::Signal(const char* file_name){
 	//opens the given input file for reading
 
 	if (fp == NULL) { //making sure the default input file opens correctly
-		printf("Error opening the default input file Raw_data_01.txt");
-		//return 0;
-		//terminates program
+		cout<<"Error opening the default input file"<<file_name<<endl;
+		
 	}
 
 	fscanf(fp, "%d %d", &length, &max);
@@ -249,7 +218,7 @@ void Signal::normal(){
 }
 
 void Signal::Sig_info(){
-	cout << "\nLength: " << length << endl << "\nMax: " << max << endl << "Average" << mean << endl; //prints out signal info
+	cout << "\nLength: " << length << endl << "\nMax: " << max << endl << "Average: " << mean << endl; //prints out signal info
 }
 
 void Signal::Print_signal(){
@@ -283,71 +252,36 @@ int main(int argc, char *argv[]) {
 	
 	
 	int file_sel=0;
-	
-	/*
-	int* length = malloc(sizeof(int));
-	int* max_val = malloc(sizeof(int));
-	*/
+	int choice=0;
+	int choice_input_type=0;
+
 	
 	//defines all of our file name strings
 	char file_name[66];
 	char new_file_name[66];
 	
-	/*
-	char stat_file[73];
-	char offset_file[69];
-	char scaled_file[69];
-	char center_file[72];
-	char normal_file[73];
-	char temp_file_name[16];
-	*/
-	
-	//defines the counter and all the flags for error checking
-	int i=1;
-	int n_flag=0;
-	int r_flag=0;
-	
 	Signal sig1;
 	
-	double offset_val,scale_val;//vlaues to store scale and offsets
-	char* ptr_1;//empty pointer needed for strtod to work
-	char* new_name;//pointer to hold the new file name from user if needed.
-	
+	double offset_val,scale_val;//vlaues to store scale and offsets	
 	
 if (argc==3){
 	
 		if(argv[1][0]=='-' && argv[1][1]=='n'){
-			if(isNumeric(argv[2])==0){//checks to make sure the value given is a number
-				n_flag=-1;
-				//goto print_outs;//if the user doesn't give a file number no action can be taken so we go to help screen and print out message
-			}
-			else{
 				file_sel=atoi(argv[2]);//happens if user gives a valid input
-				n_flag=1;
-				i++;
-			}
+				sig1.builder(file_sel);	
 		}
+		
 		else if(argv[1][0]=='-' && argv[1][1]=='f'){
-				i++;
-				if(1<argc){//makes sure that there is something after the "-n". Failure to check for this could cause Segmentation Faults
-				
-				char new_name[66];
-				sprintf(new_name,"%s",argv[i]);
-				r_flag=1;
-				}
-				else
-					r_flag=-1;
+				sprintf(new_file_name,"%s",argv[2]);
+				sig1.builder(new_file_name);
 		}
 		else	
-			printf("in the argc==3 part");
-			
+				cout<<endl<<"The formatting of the command line arguments is incorrect\nYou can only have one tag, either '-f or '-n'.\n";
 	
 	}
 
 
-else{
-	int choice=0;
-	int choice_input_type=0;
+else if(argc==1){
 	
 	cout<<endl<<"Would you like to\n(1) Enter a filename\n(2) Enter a file number\n(3) Select the default case"<<endl;
 	cin>>choice_input_type;
@@ -362,14 +296,21 @@ else{
 		sig1.builder(file_sel);
 	}
 	else{
-		printf("before build\n");
 		sig1.builder();
-		printf("after build\n");
 	}
-	printf("after loop\n");
+	
+}
+else{
+	cout<<endl<<"You entered an incorrect number of command line arguments\nYou may either have no command line arguements or you may have 1 tag ('-f'or '-n') followed by the filename or file number, respectively\n";
+}
+	
+	if(result==0){
+		return 0;
+	}
 	while(choice!=8){
 		cout << endl << "Please select an option"<<endl<<"(1):offset signal\n(2):scale signal\n(3):center signal\n(4):normalize signal\n(5):Display Statistics\n(6):Print Signal\n(7):Save Signal\n(8):Exit\n";
 		cin>>choice;
+		cout<<endl;
 	switch(choice){
 		case 1:
 				cout<<"Please enter an offset value:";
@@ -393,7 +334,7 @@ else{
 				cout<<"Signal has been normalized"<<endl;
 				break;
 		case 5:
-				sig1.stats();
+				sig1.Sig_info();
 				cout<<"Signal Statistics have been printed out"<<endl;
 				break;
 		case 6:
@@ -418,99 +359,8 @@ else{
 	}//end of switch statement		
 		
 	}//end of while
-	
-	
 
-	
-}
-/*
-	//defines strings for our data 
-	int* array;
-	double* array_changed;
-	
-	if(n_flag!=1){//makes sure that the user gives a file number
-		goto print_outs;
-	}
-	
-	if(r_flag!=1){
-	//this is run if no filename was selected to be renamed
-	//actually creates the file names to be used
-		if (file_sel < 10) {
-		sprintf(file_name, "Raw_data_0%d.txt", file_sel);
-		
-		} 
-		else {
-		sprintf(file_name, "Raw_data_%d.txt", file_sel);
-		
-		
-		}
-	}
-	else{
-	//this is run if a new filename was selected	
-		
-		//creates the new file names to be used by the program
-		sprintf(file_name, "%s.txt", new_name);
-	
-	}//end of else for the rename part
-
-	//loads the array from the input file
-	array = load_array(file_name, length, max_val);
-
-	
-	
-print_outs:	
-	
-	//frees the memory we allocated for our strings. 
-	free(array);
-	
-	//Prints out error message if certain user errors were made
-	if(n_flag!=1)
-		printf("\nProgram did not receive a a file number to read from\n");
-	if(o_flag==-1)
-		printf("\nProgram did not receive a valid offset value from the user\nProgram was not able to perform this function\n");
-	if(s_flag==-1)
-		printf("\nProgram did not receive a valid scale value from the user\nProgram was not able to perform this function\n");
-	if(r_flag==-1)
-		printf("\nProgram could not find a new name to name the files\nAll other commands were executed using the default file name\n");
-	if(r_flag==-1||s_flag==-1||o_flag==-1||n_flag!=1){
-	printf("\n\n\nSince you had some trouble with the formatting we thought you might like some help\n\nYou have the following options to put in your command line:\n-n <File Number>	Selects the file number which you would like to open\n-o <Offset Value>	Lets you instruct the program to offset the data and lets you offset it by the value you choose\n-s <Scale Factor>	Lets you instruct the program to scale the data and lets you scale it by the value you choose\n-S	Lets you instruct the program to get the statistics from the data\n-C	Lets you instruct the program to Center the data points\n-N	Lets you instruct the program to Normalize the data points\n-r <NewName>	Lets you create a copy of the input file you select and then formats the output files to this name\n-h	Lets you display the help menu\n\nThe following are example calls\n./a.exe -f 2 -o 43 -S\n./a.exe -f 2 -s -2.5 -C\n./a.exe -f 11 -r NewName1 -N\n\nEven though you had some incorrect formatting we executed as many commands as possible\n");	
-	}
-	*/
 	return 0;
 
 } //end of main
-/*
-int *load_array(char* file, int* length, int* max_val) {
-	//function to load the data from the input file in the array
-	FILE* fp = fopen(file, "r");
-	//opens the given input file for reading
 
-	if (fp == NULL) { //making sure the input file opens correctly
-		printf("Error opening input file");
-		return 0;
-		//terminates program
-	}
-
-	fscanf(fp, "%d %d", length, max_val);
-
-	int* array = malloc(sizeof(int) * *length);
-	//mallocs space for the array
-
-	int i = 0;
-	for (i = 0; i < *length; i++) {
-		//for loop to put the values into the array
-		fscanf(fp, "%d", (array + i));
-	}
-	fclose(fp);
-	return array;
-}
-*/
-
-int isNumeric (const char * s)//determines if a given string is a numeric value or not, returns 0 if it is not and non-0 value if it is a numeric value.
-{
-    if (s == NULL || *s == '\0' || isspace(*s))
-      return 0;
-    char * p;
-    strtod (s, &p);
-    return *p == '\0';
-}
